2df4441ae5043586dd9c28c44c4a8598f00d3016
ephraim lifanjo <ephraimlifanjos@gmail.com>
2026-08-25 16:57:25 +0100

    seo: allow major search social and AI crawlers

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,34 @@
+const allowedBots = [
+  "Googlebot",
+  "GoogleOther",
+  "Bingbot",
+  "DuckDuckBot",
+  "Applebot",
+  "YandexBot",
+  "Baiduspider",
+  "Bravebot",
+  "Qwantify",
+  "Slurp",
+  "facebookexternalhit",
+  "Twitterbot",
+  "LinkedInBot",
+  "Discordbot",
+  "Slackbot",
+  "WhatsApp",
+  "GPTBot",
+  "OAI-SearchBot",
+  "ChatGPT-User",
+  "PerplexityBot",
+  "ClaudeBot",
+];
+
 export default function robots() {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      { userAgent: "*", allow: "/" },
+      ...allowedBots.map((userAgent) => ({ userAgent, allow: "/" })),
+    ],
     sitemap: "https://ephraimlifanjo.vercel.app/sitemap.xml",
-    host: "https://ephraimlifanjo.vercel.app",
+    host: "ephraimlifanjo.vercel.app",
   };
 }
